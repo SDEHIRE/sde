f9a5b87b13e88e24d5d5f56284d4368c0f87d06d
ASHISH-RATNA-DARLA <darlaashishratna@gmail.com>
2024-11-07 15:01:28 +0530

    Updated existing functionality to improve performance

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
+// tailwind.config.js
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // adjust this line to match all your content
+    "./public/index.html"
+  ],
+  darkMode: 'media', // Adjust dark mode setting
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
 }
-  
